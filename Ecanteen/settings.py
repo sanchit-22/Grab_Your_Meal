dfa196fb159a8dd4315e431e1d9a400b01810479
Sanchit Kumar <59304167+sanchit-22@users.noreply.github.com>
2021-04-17 13:47:30 +0530

    Update settings.py

--- Ecanteen/settings.py
+++ Ecanteen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=wf6v3kz!4i8q+5wzef1h0$@68p*d#-6(snpa7sz0g(d*m&8$q'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
